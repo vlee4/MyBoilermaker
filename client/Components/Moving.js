d7937aa715e8625f7c2cc186588b3d3e156fd24f
Victoria <vicglee1234007@gmail.com>
2020-04-17 09:32:21 -0500

    deleted some comments

--- client/Components/Moving.js
+++ client/Components/Moving.js
@@ -8,9 +8,6 @@ export class Moving extends React.Component {
   }
 
   componentDidMount() {
-    // Wrap every letter in a span
-    // var Mog = document.querySelector(".Mog");
-
     anime({
       targets: ".Mog",
       keyframes: [
@@ -24,30 +21,6 @@ export class Moving extends React.Component {
       easing: "easeOutElastic(1, .8)",
       loop: true,
     });
-    // var textWrapper = document.querySelector(".ml7 .letters");
-    // textWrapper.innerHTML = textWrapper.textContent.replace(
-    //   /\S/g,
-    //   "<span class='letter'>$&</span>"
-    // );
-    // anime
-    //   .timeline({ loop: true })
-    //   .add({
-    //     targets: ".ml7 .letter",
-    //     translateY: ["1.1em", 0],
-    //     translateX: ["0.55em", 0],
-    //     translateZ: 0,
-    //     rotateZ: [180, 0],
-    //     duration: 750,
-    //     easing: "easeOutExpo",
-    //     delay: (el, i) => 50 * i,
-    //   })
-    //   .add({
-    //     targets: ".ml7",
-    //     opacity: 0,
-    //     duration: 1000,
-    //     easing: "easeOutExpo",
-    //     delay: 1000,
-    //   });
   }
 
   render() {
@@ -60,10 +33,3 @@ export class Moving extends React.Component {
   }
 }
 export default connect()(Moving);
-{
-  /* <h1 className="ml7">
-        <span className="text-wrapper">
-          <span className="letters">Reality is broken</span>
-        </span>
-      </h1> */
-}
